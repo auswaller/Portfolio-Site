08923f7b900658b1e3f4237b80c031c7845c17eb
Austin W <auswaller@gmail.com>
2023-08-07 12:11:47 -0400

    potential fix for blank page4

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/portfolio-site-dist/',
   plugins: [react()],
-  server: {
-    port: 3002,
-    open: true
-  }
 })
