1886c70799d952ae3cff0f6bbdeacce5433fd6be
Austin W <auswaller@gmail.com>
2023-08-07 16:05:09 -0400

    added router functionality and error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import PageNav from './components/PageNav';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <PageNav />
+      <Outlet />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ResumePage from './pages/PortfolioPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+  <RouterProvider router={router} />
+);
