ecc86f4eb322488d44d7769dc1bd0934c2b55ad4
Austin W <auswaller@gmail.com>
2023-08-07 10:39:58 -0400

    added react-bootstrap and started navbar

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar fixed="top" expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#about-me">Austin Waller</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#about-me">About Me</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
